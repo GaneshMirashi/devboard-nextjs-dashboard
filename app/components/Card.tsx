d459d4b7572a43cacfbc28891fb85c97bb9900ed
Ganesh-Mirashi <ganesh.mirashi@ascenddefi.com>
2026-04-09 12:08:50 +0530

    Created project structure.

--- app/components/Card.tsx
+++
@@ -1,8 +0,0 @@
-export default function Card({ title, value }) {
-  return (
-    <div className="p-5 bg-white rounded-2xl shadow hover:shadow-lg transition">
-      <h3 className="text-gray-500">{title}</h3>
-      <p className="text-2xl font-bold mt-2">{value}</p>
-    </div>
-  );
-}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/app/styles/globals.css"
+import "@/app/styles/layout.css"
+import "@/app/styles/components.css"
+import "@/app/styles/utilities.css"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
